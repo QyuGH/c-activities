f68dc5f1219af086271c5f79889ad53a3043a742
QyuGH <markanton.cahutay@students.isatu.edu.ph>
2024-11-01 21:40:17 +0800

    added another algorithm

--- sortingAlgorithms.c
+++ sortingAlgorithms.c
@@ -4,22 +4,33 @@
 
 void generateArray(int arr[], int n);
 void selectionSort(int arr[], int n);
+void insertionSort(int arr[], int n);
 void getRandomNum(int arr[], int n, int random);
 void printArray(int arr[], int n);
 
 int main() {
-    int n = 160000;
+    int n = 80000;
     int arr[n];
 
     srand(time(0));
 
+    //selection sort
     generateArray(arr, n);
-
+    clock_t start = clock();
     selectionSort(arr, n);
+    clock_t end = clock();
+    double selectionTime = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("Time taken for selection sort is: %fseconds\n", selectionTime);
 
-    int random = 160000;
-    printf("Random number to search for: %d\n", random);
-    getRandomNum(arr, n, random);
+
+    generateArray(arr, n);
+    start = clock();
+    insertionSort(arr, n);
+    end = clock();
+    double insertionTime = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("Time taken for insertion sort is: %fseconds\n", insertionTime);
+
+
 
     return 0;
 }
@@ -50,6 +61,22 @@ void selectionSort(int arr[], int n) {
     }
 }
 
+void insertionSort(int arr[], int n){
+    for (int i = 1; i < n; i++){
+        int insert = i;
+        int currentVal = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > currentVal){
+            arr[j + 1] = arr[j];
+            insert = j;
+            j--;
+        }
+
+        arr[insert] = currentVal;
+    }
+}
+
 void getRandomNum(int arr[], int n, int random) {
     int count = 0;
     for (int i = 0; i < n; i++) {
@@ -68,3 +95,4 @@ void printArray(int arr[], int n) {
     }
     printf("\n");
 }
+
